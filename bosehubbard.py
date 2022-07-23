0cddc137ce214bce91b6e8597c016a48e1044277
gregormedos <medos.gregor@gmail.com>
2022-07-23 13:33:49 +0200

    Added maximum occupancy in the Hamiltonian construction, joined operators to the HilbertSpace class

--- bosehubbard.py
+++ bosehubbard.py
@@ -94,95 +94,54 @@ def gen_basis_nmax(N, L, Nmax):
     return basis
 
 
-# lowering operator
-def op_lower(i, fock):
-    copy = np.copy(fock)
-    copy[i] -= 1
-    return copy
-
-def op_lower_n(i, n, fock):
-    copy = np.copy(fock)
-    if n == 0:
-        return copy
-    else:
-        return op_lower(i, op_lower_n(i, n - 1, copy))
-
-def op_b(i, fock):
-    n = fock[i]
-    return np.sqrt(n) * op_lower(i, fock)
-
-def op_b_n(i, n, fock):
-    copy = np.copy(fock)
-    if n == 0:
-        return copy
-    else:
-        return op_b(i, op_b_n(i, n - 1, copy))
-
-
-# raising operator
-def op_raise(i, fock):
-    copy = np.copy(fock)
-    copy[i] += 1
-    return copy
-
-def op_raise_n(i, n, fock):
-    copy = np.copy(fock)
-    if n == 0:
-        return copy
-    else:
-        return op_raise(i, op_raise_n(i, n - 1, copy))
-
-def op_bdagger(i, fock):
-    n = fock[i]
-    return np.sqrt(n + 1) * op_raise(i, fock)
-
-def op_bdagger_n(i, n, fock):
-    copy = np.copy(fock)
-    if n == 0:
-        return copy
-    else:
-        return op_bdagger(i, op_bdagger_n(i, n - 1, copy))
-
-
 # create a Hilbert space
 class HilbertSpace:
-    def __init__(self, N, L):
+    def __init__(self, N, L, Nmax):
         self.N = N                                              # total number of quanta
         self.L = L                                              # number of sites
-        self.basis = gen_basis_nmax(self.N, self.L, self.N)     # N-block basis
+        self.Nmax = Nmax                                        # maximum occupancy for any site
+        self.basis = gen_basis_nmax(N, L, Nmax)                 # N-block basis
         self.D = len(self.basis)                                # dimension of basis
         self.map = dict()
         for j in range(self.D):
             self.map[tuple(self.basis[j])] = j                  # mapping fock states to indices
 
 
+    # given index return fock state
     def get_fock_state(self, j):
         fock = np.copy(self.basis[j])
         return fock
 
 
-    def op_kinetic_pbc(self, t):
-        new_states = np.zeros((self.D, self.D), dtype=float)
-        for j in range(self.D):
-            fock = self.get_fock_state(j)
-            for i in range(self.L):
-                if fock[i] > 0:
-                    amplitude1 = fock[i]
-                    lower_fock = op_lower(i, fock)
+    # lowering operator
+    def op_lower(self, i, fock):
+        copy = np.copy(fock)
+        copy[i] -= 1
+        return copy
 
-                    indeks = (i + 1) % self.L # PBC
-                    amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                    new_fock = op_raise(indeks, lower_fock)
-                    new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
-
-                    indeks = (i + self.L - 1) % self.L # PBC
-                    amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                    new_fock = op_raise(indeks, lower_fock)
-                    new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
-
-        return new_states
+    def op_lower_n(self, i, n, fock):
+        copy = np.copy(fock)
+        if n == 0:
+            return copy
+        else:
+            return self.op_lower(i, self.op_lower_n(i, n - 1, copy))
 
 
+    # raising operator
+    def op_raise(self, i, fock):
+        copy = np.copy(fock)
+        copy[i] += 1
+        return copy
+
+    def op_raise_n(self, i, n, fock):
+        copy = np.copy(fock)
+        if n == 0:
+            return copy
+        else:
+            return self.op_raise(i, self.op_raise_n(i, n - 1, copy))
+
+
+    # Coulomb interaction
     def op_interaction(self, U):
         new_states = np.zeros((self.D, self.D), dtype=float)
         for j in range(self.D):
@@ -196,6 +155,30 @@ class HilbertSpace:
         return new_states
 
 
+    # Hamiltonian with PBC
+    def op_kinetic_pbc(self, t):
+        new_states = np.zeros((self.D, self.D), dtype=float)
+        for j in range(self.D):
+            fock = self.get_fock_state(j)
+            for i in range(self.L):
+                if fock[i] > 0:
+                    amplitude1 = fock[i]
+                    lower_fock = self.op_lower(i, fock)
+
+                    indeks = (i + 1) % self.L # PBC
+                    if lower_fock[indeks] < self.Nmax:
+                        amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
+                        new_fock = self.op_raise(indeks, lower_fock)
+                        new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+
+                    indeks = (i + self.L - 1) % self.L # PBC
+                    if lower_fock[indeks] < self.Nmax:
+                        amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
+                        new_fock = self.op_raise(indeks, lower_fock)
+                        new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+
+        return new_states
+
     def op_hamiltonian_pbc(self, t, inter, U):
         if inter:
             return self.op_kinetic_pbc(t) + self.op_interaction(U)
@@ -203,6 +186,7 @@ class HilbertSpace:
             return self.op_kinetic_pbc(t)
 
 
+    # Hamiltonian with OBC
     def op_kinetic_obc(self, t):
         new_states = np.zeros((self.D, self.D), dtype=float)
         for j in range(self.D):
@@ -210,34 +194,38 @@ class HilbertSpace:
 
             if fock[0] > 0:
                 amplitude1 = fock[0]
-                lower_fock = op_lower(0, fock)
+                lower_fock = self.op_lower(0, fock)
                 indeks = 1 # OBC
-                amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                new_fock = op_raise(indeks, lower_fock)
-                new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+                if fock[indeks] < self.Nmax:
+                    amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
+                    new_fock = self.op_raise(indeks, lower_fock)
+                    new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
 
             if fock[self.L - 1] > 0:
                 amplitude1 = fock[self.L - 1]
-                lower_fock = op_lower(self.L - 1, fock)
+                lower_fock = self.op_lower(self.L - 1, fock)
                 indeks = self.L - 2 # OBC
-                amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                new_fock = op_raise(indeks, lower_fock)
-                new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+                if fock[indeks] < self.Nmax:
+                    amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
+                    new_fock = self.op_raise(indeks, lower_fock)
+                    new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
                 
             for i in range(1, self.L - 1):
                 if fock[i] > 0:
                     amplitude1 = fock[i]
-                    lower_fock = op_lower(i, fock)
+                    lower_fock = self.op_lower(i, fock)
 
                     indeks = i + 1 # OBC
-                    amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                    new_fock = op_raise(indeks, lower_fock)
-                    new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+                    if fock[indeks] < self.Nmax:
+                        amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
+                        new_fock = self.op_raise(indeks, lower_fock)
+                        new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
 
                     indeks = i - 1 # OBC
-                    amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                    new_fock = op_raise(indeks, lower_fock)
-                    new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+                    if fock[indeks] < self.Nmax:
+                        amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
+                        new_fock = self.op_raise(indeks, lower_fock)
+                        new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
 
         return new_states
 
