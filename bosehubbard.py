dd7e5a5c20337a7d46bcd81bf0254d289061b662
gregormedos <medos.gregor@gmail.com>
2022-08-05 00:32:37 +0200

    Implemented diagonalization per k-block for each N-block, smaller memory chunks needed

--- bosehubbard.py
+++ bosehubbard.py
@@ -6,42 +6,28 @@ import numpy as np
 ## lowering operator
 #-----------------------------------------------------------------------------------------------
 def op_lower(i, fock):
-    copy = np.copy(fock)
-    copy[i] -= 1
-    return copy
-
-def op_lower_n(i, n, fock):
-    copy = np.copy(fock)
-    if n == 0:
-        return copy
-    else:
-        return op_lower(i, op_lower_n(i, n - 1, copy))
+    new_fock = np.copy(fock)
+    new_fock[i] -= 1
+    return new_fock
 
 
 #-----------------------------------------------------------------------------------------------
 ## raising operator
 #-----------------------------------------------------------------------------------------------
 def op_raise(i, fock):
-    copy = np.copy(fock)
-    copy[i] += 1
-    return copy
-
-def op_raise_n(i, n, fock):
-    copy = np.copy(fock)
-    if n == 0:
-        return copy
-    else:
-        return op_raise(i, op_raise_n(i, n - 1, copy))
+    new_fock = np.copy(fock)
+    new_fock[i] += 1
+    return new_fock
 
 
 #-----------------------------------------------------------------------------------------------
 ## translation operator
 #-----------------------------------------------------------------------------------------------
 def op_translation(fock, Lsites):
-    copy = np.copy(fock)
+    new_fock = np.copy(fock)
     for i in range(Lsites):
-        copy[i] = fock[(i - 1) % Lsites] # PBC
-    return copy
+        new_fock[i] = fock[(i - 1) % Lsites] # PBC
+    return new_fock
 
 
 #-----------------------------------------------------------------------------------------------
@@ -62,13 +48,13 @@ def dim_nblock(Lsites, Nquanta):
 def gen_basis_nblock(Lsites, Nquanta, Dim):
     if Lsites > 1:
         basis = np.zeros((Dim, Lsites), dtype=int)
-        j = 0
+        a = 0
         for n in range(Nquanta + 1):
             l = Lsites - 1
             d = dim_nblock(n, l)
-            basis[j:j + d, 0] = Nquanta - n
-            basis[j:j + d, 1:] = gen_basis_nblock(l, n, d)
-            j += d
+            basis[a:a + d, 0] = Nquanta - n
+            basis[a:a + d, 1:] = gen_basis_nblock(l, n, d)
+            a += d
     else:
         basis = np.array([Nquanta], dtype=int)
     
@@ -84,13 +70,13 @@ def gen_basis_nmax(Lsites, Nmax):
     if Lsites > 1:
         Dim = (Nmax + 1)**Lsites
         basis = np.zeros((Dim, Lsites), dtype=int)
-        j = 0
+        a = 0
         for n in range(Nmax + 1):
             l = Lsites - 1
             d = Dim // (Nmax + 1)
-            basis[j:j + d, 0] = n
-            basis[j:j + d, 1:] = gen_basis_nmax(l, Nmax)
-            j += d
+            basis[a:a + d, 0] = n
+            basis[a:a + d, 1:] = gen_basis_nmax(l, Nmax)
+            a += d
     else:
         Dim = Nmax + 1
         basis = np.zeros((Dim, 1), dtype=int)
@@ -134,8 +120,8 @@ def gen_basis_n_nmax(Lsites, Nmax, Nquanta):
             block_subbasis = gen_basis_n_nmax(Lsites - 1, Nmax, Nquanta - n)
             d = len(block_subbasis)
             block_basis = np.zeros((d, Lsites), dtype=int)
-            for j in range(d):
-                block_basis[j, 0] = n
+            for a in range(d):
+                block_basis[a, 0] = n
                 block_basis[:, 1:] = block_subbasis
             
             basis_list.append(block_basis)
@@ -155,41 +141,36 @@ def gen_basis_n_nmax(Lsites, Nmax, Nquanta):
 
 
 #-----------------------------------------------------------------------------------------------
-## generate basis with translational symmetry (K)
+## generate basis with translational symmetry (k)
 ## with const. N nd maximum occupancy for every site
 # Lsites = number of sites
 # Nmax = maximum occupancy for every site
 # Nquanta = total number of quanta
 #-----------------------------------------------------------------------------------------------
-def gen_basis_k_n_nmax(Lsites, Nmax, Nquanta):
+def gen_basis_k_n_nmax(Lsites, Nmax, Nquanta, Kmoment):
     basis_raw = gen_basis_n_nmax(Lsites, Nmax, Nquanta)
     d = len(basis_raw)
     basis_list = list()
     periodicity_list = list()
-    for j in range(d):
+    for a in range(d):
         Period = -1
-        state = np.copy(basis_raw[j])
+        state = np.copy(basis_raw[a])
         for i in range(1, Lsites + 1):
             state = op_translation(state, Lsites)
-            if tuple(state) < tuple(basis_raw[j]):
+            if tuple(state) < tuple(basis_raw[a]):
                 break
-            elif tuple(state) == tuple(basis_raw[j]):
-                Period = i
-                break
+            elif tuple(state) == tuple(basis_raw[a]):
+                if (Kmoment % (Lsites / i)) == 0:
+                    Period = i
+                    break
+                else:
+                    break
     
         if Period >= 0:
             basis_list.append(state)
             periodicity_list.append(Period)
 
     return (np.array(basis_list, dtype=int), np.array(periodicity_list, dtype=int))
-
-def gen_k_space(Period, Lsites):
-    k_list = list()
-    for k in range(Lsites):
-        if (k % (Lsites / Period)) == 0:
-            k_list.append(k)
-    
-    return np.array(k_list, dtype=int)
 
 
 #-----------------------------------------------------------------------------------------------
@@ -200,86 +181,94 @@ def gen_k_space(Period, Lsites):
 # Nquanta = total number of quanta
 #-----------------------------------------------------------------------------------------------
 class HilbertSpace:
-    def __init__(self, Lsites, Nmax, Sym, Nquanta):
+    ## construct Hilbert space with given parameters
+    def __init__(self, Lsites, Nmax, Sym=None, Nquanta=None, Kmoment=None):
         self.Lsites = Lsites                                               # number of sites
         self.Nmax = Nmax                                                   # maximum occupancy for any site
-        if Sym == 'KN':
-            self.Nquanta = Nquanta                                                         # total number of quanta
-            self.basis, self.periodicities = gen_basis_k_n_nmax(Lsites, Nmax, Nquanta)     # KN basis
+        if Sym == 'kN':
+            self.Nquanta = Nquanta                                                                  # total number of quanta
+            self.Kmoment = Kmoment                                                                  # cyrstal momentum k
+            self.basis, self.periodicities = gen_basis_k_n_nmax(Lsites, Nmax, Nquanta, Kmoment)     # kN basis
+
         elif Sym == 'N':
-            self.Nquanta = Nquanta                                         # total number of quanta
+            self.Nquanta = Nquanta                                         # total number of quanta N
             self.basis = gen_basis_n_nmax(Lsites, Nmax, Nquanta)           # N basis
+
         else:
             self.basis = gen_basis_nmax(Lsites, Nmax)                      # full basis
         
-        self.Dim = len(self.basis)                                         # dimension of basis
+        self.Dim = len(self.basis)                                         # dimension of Hilbert space
         self.map = dict()
-        for j in range(self.Dim):
-            self.map[tuple(self.basis[j])] = j                             # mapping fock states to indices
+        for a in range(self.Dim):
+            self.map[tuple(self.basis[a])] = a                             # mapping fock states to indices
 
 
-    # given index return fock state
+    ## given index return fock state
     def get_fock_state(self, j):
         fock = np.copy(self.basis[j])
         return fock
 
 
-    # Coulomb interaction
+    ## Coulomb interaction
     def op_interaction(self, U):
-        new_states = np.zeros((self.Dim, self.Dim), dtype=float)
-        for j in range(self.Dim):
-            fock = self.get_fock_state(j)
+        matrika = np.zeros((self.Dim, self.Dim), dtype=float)
+        for a in range(self.Dim):
+            fock = self.get_fock_state(a)
             amplitude = 0.0
             for i in range(self.Lsites):
                 amplitude += fock[i] * (fock[i] - 1)
             
-            new_states[j, j] = 0.5 * U * amplitude
+            matrika[a, a] = 0.5 * U * amplitude
 
-        return new_states
+        return matrika
 
 
-    # Hamiltonian with OBC
+    ## Hamiltonian with OBC
     def op_kinetic_obc(self, t):
-        new_states = np.zeros((self.Dim, self.Dim), dtype=float)
-        for j in range(self.Dim):
-            fock = self.get_fock_state(j)
+        matrika = np.zeros((self.Dim, self.Dim), dtype=float)
+        for a in range(self.Dim):
+            fock_a = self.get_fock_state(a)
 
-            if fock[0] > 0:
-                amplitude1 = fock[0]
-                lower_fock = op_lower(0, fock)
-                indeks = 1 # OBC
-                if fock[indeks] < self.Nmax:
-                    amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                    new_fock = op_raise(indeks, lower_fock)
-                    new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+            if fock_a[0] > 0:
+                amplitude1 = fock_a[0]
+                lower_fock = op_lower(0, fock_a)
+                j = 1 # OBC
+                if lower_fock[j] < self.Nmax:
+                    amplitude = np.sqrt(amplitude1 * (lower_fock[j] + 1))
+                    fock_b = op_raise(j, lower_fock)
+                    b = self.map[tuple(fock_b)]
+                    matrika[a, b] += - t * amplitude
 
-            if fock[self.Lsites - 1] > 0:
-                amplitude1 = fock[self.Lsites - 1]
-                lower_fock = op_lower(self.Lsites - 1, fock)
-                indeks = self.Lsites - 2 # OBC
-                if fock[indeks] < self.Nmax:
-                    amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                    new_fock = op_raise(indeks, lower_fock)
-                    new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+            if fock_a[self.Lsites - 1] > 0:
+                amplitude1 = fock_a[self.Lsites - 1]
+                lower_fock = op_lower(self.Lsites - 1, fock_a)
+                j = self.Lsites - 2 # OBC
+                if lower_fock[j] < self.Nmax:
+                    amplitude = np.sqrt(amplitude1 * (lower_fock[j] + 1))
+                    fock_b = op_raise(j, lower_fock)
+                    b = self.map[tuple(fock_b)]
+                    matrika[a, b] += - t * amplitude
                 
             for i in range(1, self.Lsites - 1):
-                if fock[i] > 0:
-                    amplitude1 = fock[i]
-                    lower_fock = op_lower(i, fock)
+                if fock_a[i] > 0:
+                    amplitude1 = fock_a[i]
+                    lower_fock = op_lower(i, fock_a)
 
-                    indeks = i + 1 # OBC
-                    if fock[indeks] < self.Nmax:
-                        amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                        new_fock = op_raise(indeks, lower_fock)
-                        new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+                    j = i + 1 # OBC
+                    if lower_fock[j] < self.Nmax:
+                        amplitude = np.sqrt(amplitude1 * (lower_fock[j] + 1))
+                        fock_b = op_raise(j, lower_fock)
+                        b = self.map[tuple(fock_b)]
+                        matrika[a, b] += - t * amplitude
 
-                    indeks = i - 1 # OBC
-                    if fock[indeks] < self.Nmax:
-                        amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                        new_fock = op_raise(indeks, lower_fock)
-                        new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+                    j = i - 1 # OBC
+                    if lower_fock[j] < self.Nmax:
+                        amplitude = np.sqrt(amplitude1 * (lower_fock[j] + 1))
+                        fock_b = op_raise(j, lower_fock)
+                        b = self.map[tuple(fock_b)]
+                        matrika[a, b] += - t * amplitude
 
-        return new_states
+        return matrika
 
     def op_hamiltonian_obc(self, t, inter, U):
         if inter:
@@ -288,35 +277,107 @@ class HilbertSpace:
             return self.op_kinetic_obc(t)
 
 
-    # Hamiltonian with PBC
+    ## Hamiltonian with PBC
     def op_kinetic_pbc(self, t):
-        new_states = np.zeros((self.Dim, self.Dim), dtype=float)
-        for j in range(self.Dim):
-            fock = self.get_fock_state(j)
+        matrika = np.zeros((self.Dim, self.Dim), dtype=float)
+        for a in range(self.Dim):
+            fock_a = self.get_fock_state(a)
             for i in range(self.Lsites):
-                if fock[i] > 0:
-                    amplitude1 = fock[i]
-                    lower_fock = op_lower(i, fock)
+                if fock_a[i] > 0:
+                    amplitude1 = fock_a[i]
+                    lower_fock = op_lower(i, fock_a)
 
-                    indeks = (i + 1) % self.Lsites # PBC
-                    if lower_fock[indeks] < self.Nmax:
-                        amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                        new_fock = op_raise(indeks, lower_fock)
-                        new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+                    j = (i + 1) % self.Lsites # PBC
+                    if lower_fock[j] < self.Nmax:
+                        amplitude = np.sqrt(amplitude1 * (lower_fock[j] + 1))
+                        fock_b = op_raise(j, lower_fock)
+                        b = self.map[tuple(fock_b)]
+                        matrika[a, b] += - t * amplitude
 
-                    indeks = (i + self.Lsites - 1) % self.Lsites # PBC
-                    if lower_fock[indeks] < self.Nmax:
-                        amplitude = np.sqrt(amplitude1 * (lower_fock[indeks] + 1))
-                        new_fock = op_raise(indeks, lower_fock)
-                        new_states[j, self.map[tuple(new_fock)]] += - t * amplitude
+                    j = (i + self.Lsites - 1) % self.Lsites # PBC
+                    if lower_fock[j] < self.Nmax:
+                        amplitude = np.sqrt(amplitude1 * (lower_fock[j] + 1))
+                        fock_b = op_raise(j, lower_fock)
+                        b = self.map[tuple(fock_b)]
+                        matrika[a, b] += - t * amplitude
 
-        return new_states
+        return matrika
 
     def op_hamiltonian_pbc(self, t, inter, U):
         if inter:
             return self.op_kinetic_pbc(t) + self.op_interaction(U)
         else:
             return self.op_kinetic_pbc(t)
+
+
+    ## Coulomb interaction for k-block Hamiltonian
+    def op_interaction_k(self, U):
+        matrika = np.zeros((self.Dim, self.Dim), dtype=float)
+        for a in range(self.Dim):
+            fock = self.get_fock_state(a)
+            amplitude = 0.0
+            for i in range(self.Lsites):
+                amplitude += fock[i] * (fock[i] - 1)
+            
+            matrika[a, a] = 0.5 * U * amplitude
+
+        return matrika
+
+
+    ## find representative state for states linked by translations
+    def representative(self, fock):
+        rep_fock = np.copy(fock)
+        new_fock = np.copy(fock)
+        Phase = 0
+        for i in range(self.Lsites):
+            new_fock = op_translation(new_fock, self.Lsites)
+            if tuple(new_fock) < tuple(rep_fock):
+                rep_fock = np.copy(new_fock)
+                Phase = i
+        
+        return (rep_fock, Phase)
+
+
+    ## k-block Hamiltonian
+    def op_kinetic_k(self, t):
+        matrika = np.zeros((self.Dim, self.Dim), dtype=complex)
+        for a in range(self.Dim):
+            fock_a = self.get_fock_state(a)
+            Period_a = self.periodicities[a]
+            for i in range(self.Lsites):
+                if fock_a[i] > 0:
+                    amplitude1 = fock_a[i]
+                    lower_fock = op_lower(i, fock_a)
+
+                    j = (i + 1) % self.Lsites # PBC
+                    if lower_fock[j] < self.Nmax:
+                        new_fock = op_raise(j, lower_fock)
+                        fock_b, Phase = self.representative(new_fock)
+                        if tuple(fock_b) in self.map:
+                            b = self.map[tuple(fock_b)]
+                            Period_b = self.periodicities[b]
+                            amplitude = np.sqrt(amplitude1 * (lower_fock[j] + 1) * Period_a / Period_b)
+                            Arg = 2.0 * np.pi * self.Kmoment * Phase / self.Lsites
+                            matrika[a, b] += - t * amplitude * complex(np.cos(Arg), - np.sin(Arg))
+
+                    j = (i + self.Lsites - 1) % self.Lsites # PBC
+                    if lower_fock[j] < self.Nmax:
+                        new_fock = op_raise(j, lower_fock)
+                        fock_b, Phase = self.representative(new_fock)
+                        if tuple(fock_b) in self.map:
+                            b = self.map[tuple(fock_b)]
+                            Period_b = self.periodicities[b]
+                            amplitude = np.sqrt(amplitude1 * (lower_fock[j] + 1) * Period_a / Period_b)
+                            Arg = 2.0 * np.pi * self.Kmoment * Phase / self.Lsites
+                            matrika[a, b] += - t * amplitude * complex(np.cos(Arg), - np.sin(Arg))
+
+        return matrika
+
+    def op_hamiltonian_k(self, t, inter, U):
+        if inter:
+            return self.op_kinetic_k(t) + self.op_interaction_k(U)
+        else:
+            return self.op_kinetic_k(t)
 
 
 #-----------------------------------------------------------------------------------------------
