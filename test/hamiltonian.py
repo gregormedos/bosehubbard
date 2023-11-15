147a89800c4360442c7e2fa0924623bb099cf083
Gregor Medoš <medos.gregor@gmail.com>
2023-11-15 23:56:24 +0100

    Test N, K, P symmetries

--- test/hamiltonian.py
+++ test/hamiltonian.py
@@ -14,7 +14,7 @@ HAMILTONIAN_K_DICT = {
     1: bh.HilbertSpace.op_hamiltonian_tunnel_k,
     2: bh.HilbertSpace.op_hamiltonian_annihilate_create_pair_k,
 }
-PRECISION = 15
+PRECISION = 10
 BINS = 60
 
 cmap = mpl.colormaps['RdBu']
@@ -29,31 +29,28 @@ def test_symmetries(num: int):
     hs = bh.HilbertSpace(5, 2)
     h = HAMILTONIAN_DICT[num](hs)
     axes[0, 0].imshow(h, norm=CenteredNorm(), cmap=cmap)
-    w = np.linalg.eigvalsh(h)
-    w = np.round(w, PRECISION)
+    w = np.round(np.linalg.eigvalsh(h), PRECISION)
     axes[1, 0].hist(w, BINS)
     s = hs.basis_transformation_n(h)
     h = s.T @ h @ s
     axes[0, 1].imshow(h, norm=CenteredNorm(), cmap=cmap)
-    w = np.linalg.eigvalsh(h)
-    w = np.round(w, PRECISION)
+    w = np.round(np.linalg.eigvalsh(h), PRECISION)
     axes[1, 1].hist(w, BINS)
     h = HAMILTONIAN_DICT[num](hs)
     s = hs.basis_transformation_k(h)
     h = s.conj().T @ h @ s
     axes[0, 2].imshow(np.abs(h), norm=CenteredNorm(), cmap=cmap)
-    w = np.linalg.eigvalsh(h)
-    w = np.round(w, PRECISION)
+    w = np.round(np.linalg.eigvalsh(h), PRECISION)
     axes[1, 2].hist(w, BINS)
     h = HAMILTONIAN_DICT[num](hs)
     s = hs.basis_transformation_kn(h)
     h = s.conj().T @ h @ s
     axes[0, 3].imshow(np.abs(h), norm=CenteredNorm(), cmap=cmap)
-    w = np.linalg.eigvalsh(h)
-    w = np.round(w, PRECISION)
+    w = np.round(np.linalg.eigvalsh(h), PRECISION)
     axes[1, 3].hist(w, BINS)
 
     fig.tight_layout()
+    fig.savefig('test/symmetries.png')
 
 
 def test_decomposition_n(num: int):
@@ -62,23 +59,40 @@ def test_decomposition_n(num: int):
     for axis in axes.flat:
         axis.set_xticks([])
         axis.set_yticks([])
-    w = []
+
+    w_sectors = {}
     for i, hss in enumerate(hs.subspaces):
         h = HAMILTONIAN_DICT[num](hss)
         axes[i].imshow(h, norm=CenteredNorm(), cmap=cmap)
-        w.extend(np.linalg.eigvalsh(h))
+        w_sectors[f'({hss.n_tot})'] = np.round(np.linalg.eigvalsh(h), PRECISION)
 
     fig.tight_layout()
-    w = np.array(w)
-    w = np.round(w, PRECISION)
+    fig.savefig('test/decomposition_n.png')
+
+    spect = {}
+    for sector, w_sector in w_sectors.items():
+        for w in w_sector:
+            if w not in spect:
+                spect[w] = [sector, 1]
+            else:
+                spect[w][0] += sector
+                spect[w][1] += 1
+    spect = {w: spect[w] for w in sorted(spect.keys())}
+    with open('test/decomposition_n_spect.txt', 'w') as file:
+        for w, (sector, degeneracy) in spect.items():
+            file.write(f'{w:.10f}[{degeneracy}]{sector}\n')
 
     fig, axis = plt.subplots(figsize=(2.5, 2.5))
     axis.set_xticks([])
     axis.set_yticks([])
 
+    w = []
+    for w_sector in w_sectors.values():
+        w.extend(w_sector)
     axis.hist(w, BINS)
 
     fig.tight_layout()
+    fig.savefig('test/decomposition_n_spect.png')
 
 
 def test_decomposition_k(num: int):
@@ -87,23 +101,40 @@ def test_decomposition_k(num: int):
     for axis in axes.flat:
         axis.set_xticks([])
         axis.set_yticks([])
-    w = []
+
+    w_sectors = {}
     for i, hss in enumerate(hs.subspaces):
         h = HAMILTONIAN_K_DICT[num](hss)
         axes[i].imshow(np.abs(h), norm=CenteredNorm(), cmap=cmap)
-        w.extend(np.linalg.eigvalsh(h))
+        w_sectors[f'({hss.crystal_momentum})'] = np.round(np.linalg.eigvalsh(h), PRECISION)
 
     fig.tight_layout()
-    w = np.array(w)
-    w = np.round(w, PRECISION)
+    fig.savefig('test/decompostion_k.png')
+
+    spect = {}
+    for sector, w_sector in w_sectors.items():
+        for w in w_sector:
+            if w not in spect:
+                spect[w] = [sector, 1]
+            else:
+                spect[w][0] += sector
+                spect[w][1] += 1
+    spect = {w: spect[w] for w in sorted(spect.keys())}
+    with open('test/decomposition_k_spect.txt', 'w') as file:
+        for w, (sector, degeneracy) in spect.items():
+            file.write(f'{w:.10f}[{degeneracy}]{sector}\n')
 
     fig, axis = plt.subplots(figsize=(2.5, 2.5))
     axis.set_xticks([])
     axis.set_yticks([])
 
+    w = []
+    for w_sector in w_sectors.values():
+        w.extend(w_sector)
     axis.hist(w, BINS)
 
     fig.tight_layout()
+    fig.savefig('test/decompostion_k_spect.png')
 
 
 def test_decomposition_kn(num: int):
@@ -112,24 +143,41 @@ def test_decomposition_kn(num: int):
     for axis in axes.flat:
         axis.set_xticks([])
         axis.set_yticks([])
-    w = []
+
+    w_sectors = {}
     for i, hss in enumerate(hs.subspaces):
         for j, hsss in enumerate(hss.subspaces):
             h = HAMILTONIAN_K_DICT[num](hsss)
             axes[j, i].imshow(np.abs(h), norm=CenteredNorm(), cmap=cmap)
-            w.extend(np.linalg.eigvalsh(h))
+            w_sectors[f'({hsss.n_tot}|{hsss.crystal_momentum})'] = np.round(np.linalg.eigvalsh(h), PRECISION)
 
     fig.tight_layout()
-    w = np.array(w)
-    w = np.round(w, PRECISION)
+    fig.savefig('test/decompositon_kn.png')
 
-    fig, axis = plt.subplots(figsize=(5, 5))
+    spect = {}
+    for sector, w_sector in w_sectors.items():
+        for w in w_sector:
+            if w not in spect:
+                spect[w] = [sector, 1]
+            else:
+                spect[w][0] += sector
+                spect[w][1] += 1
+    spect = {w: spect[w] for w in sorted(spect.keys())}
+    with open('test/decomposition_kn_spect.txt', 'w') as file:
+        for w, (sector, degeneracy) in spect.items():
+            file.write(f'{w:.10f}[{degeneracy}]{sector}\n')
+
+    fig, axis = plt.subplots(figsize=(2.5, 2.5))
     axis.set_xticks([])
     axis.set_yticks([])
 
+    w = []
+    for w_sector in w_sectors.values():
+        w.extend(w_sector)
     axis.hist(w, BINS)
 
     fig.tight_layout()
+    fig.savefig('test/decompositon_kn_spect.png')
 
 
 def test_symmetries_k(num: int):
@@ -164,6 +212,7 @@ def test_symmetries_k(num: int):
     axes[1, 3].hist(w, BINS)
 
     fig.tight_layout()
+    fig.savefig('test/symmetries_k.png')
 
 
 def test_symmetries_kn(num: int):
@@ -198,6 +247,7 @@ def test_symmetries_kn(num: int):
     axes[1, 3].hist(w, BINS)
 
     fig.tight_layout()
+    fig.savefig('test/symmetries_kn.png')
 
 
 def test_decomposition_pk(num: int):
@@ -205,32 +255,49 @@ def test_decomposition_pk(num: int):
     for axis in axes.flat:
         axis.set_xticks([])
         axis.set_yticks([])
-    w = [[], []]
 
+    w_sectors_list = [{}, {}]
     hs = bh.DecomposedHilbertSpace(6, 2, 'K', 'PK', crystal_momentum=0)
     for i, hss in enumerate(hs.subspaces):
         h = HAMILTONIAN_K_DICT[num](hss)
         axes[0, i].imshow(np.abs(h), norm=CenteredNorm(), cmap=cmap)
-        w[0].extend(np.linalg.eigvalsh(h))
+        w_sectors_list[0][f'({hss.reflection_parity})'] = np.round(np.linalg.eigvalsh(h), PRECISION)
     hs = bh.DecomposedHilbertSpace(6, 2, 'K', 'PK', crystal_momentum=3)
     for i, hss in enumerate(hs.subspaces):
         h = HAMILTONIAN_K_DICT[num](hss)
         axes[1, i].imshow(np.abs(h), norm=CenteredNorm(), cmap=cmap)
-        w[1].extend(np.linalg.eigvalsh(h))
+        w_sectors_list[1][f'({hss.reflection_parity})'] = np.round(np.linalg.eigvalsh(h), PRECISION)
 
     fig.tight_layout()
+    fig.savefig('test/decompostion_pk.png')
+
+    for i, k in enumerate(('k=zero', 'k=bragg')):
+        spect = {}
+        for sector, w_sector in w_sectors_list[i].items():
+            for w in w_sector:
+                if w not in spect:
+                    spect[w] = [sector, 1]
+                else:
+                    spect[w][0] += sector
+                    spect[w][1] += 1
+        spect = {w: spect[w] for w in sorted(spect.keys())}
+        with open(f'test/decomposition_pk_spect_{k}.txt', 'w') as file:
+            for w, (sector, degeneracy) in spect.items():
+                file.write(f'{w:.10f}[{degeneracy}]{sector}\n')
 
     fig, axes = plt.subplots(1, 2, figsize=(5, 2.5))
     for axis in axes.flat:
         axis.set_xticks([])
         axis.set_yticks([])
 
+    w = [[], []]
     for i in range(2):
-        w[i] = np.array(w[i])
-        w[i] = np.round(w[i], PRECISION)
+        for w_sector in w_sectors_list[i].values():
+            w[i].extend(w_sector)
         axes[i].hist(w[i], BINS)
 
     fig.tight_layout()
+    fig.savefig('test/decompostion_pk_spect.png')
 
 
 def test_decomposition_pkn(num: int):
@@ -238,32 +305,49 @@ def test_decomposition_pkn(num: int):
     for axis in axes.flat:
         axis.set_xticks([])
         axis.set_yticks([])
-    w = [[], []]
 
+    w_sectors_list = [{}, {}]
     hs = bh.DecomposedHilbertSpace(6, 2, 'KN', 'PKN', n_tot=3, crystal_momentum=0)
     for i, hss in enumerate(hs.subspaces):
         h = HAMILTONIAN_K_DICT[num](hss)
         axes[0, i].imshow(np.abs(h), norm=CenteredNorm(), cmap=cmap)
-        w[0].extend(np.linalg.eigvalsh(h))
+        w_sectors_list[0][f'({hss.reflection_parity})'] = np.round(np.linalg.eigvalsh(h), PRECISION)
     hs = bh.DecomposedHilbertSpace(6, 2, 'KN', 'PKN', n_tot=3, crystal_momentum=3)
     for i, hss in enumerate(hs.subspaces):
         h = HAMILTONIAN_K_DICT[num](hss)
         axes[1, i].imshow(np.abs(h), norm=CenteredNorm(), cmap=cmap)
-        w[1].extend(np.linalg.eigvalsh(h))
+        w_sectors_list[1][f'({hss.reflection_parity})'] = np.round(np.linalg.eigvalsh(h), PRECISION)
 
     fig.tight_layout()
+    fig.savefig('test/decompostion_pkn.png')
+
+    for i, k in enumerate(('k=zero', 'k=bragg')):
+        spect = {}
+        for sector, w_sector in w_sectors_list[i].items():
+            for w in w_sector:
+                if w not in spect:
+                    spect[w] = [sector, 1]
+                else:
+                    spect[w][0] += sector
+                    spect[w][1] += 1
+        spect = {w: spect[w] for w in sorted(spect.keys())}
+        with open(f'test/decomposition_pkn_spect_{k}.txt', 'w') as file:
+            for w, (sector, degeneracy) in spect.items():
+                file.write(f'{w:.10f}[{degeneracy}]{sector}\n')
 
     fig, axes = plt.subplots(1, 2, figsize=(5, 2.5))
     for axis in axes.flat:
         axis.set_xticks([])
         axis.set_yticks([])
 
+    w = [[], []]
     for i in range(2):
-        w[i] = np.array(w[i])
-        w[i] = np.round(w[i], PRECISION)
+        for w_sector in w_sectors_list[i].values():
+            w[i].extend(w_sector)
         axes[i].hist(w[i], BINS)
 
     fig.tight_layout()
+    fig.savefig('test/decompostion_pkn_spect.png')
 
 
 def main():
@@ -285,8 +369,8 @@ def main():
     #test_decomposition_kn(2)
     test_symmetries_k(1)
     test_decomposition_pk(1)
-    test_symmetries_kn(1)
-    test_decomposition_pkn(1)
+    #test_symmetries_kn(1)
+    #test_decomposition_pkn(1)
     #test_symmetries_k(2)
     #test_decomposition_pk(2)
     #test_symmetries_kn(2)
