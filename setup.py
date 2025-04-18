09fe9406f7ed943f3856ffe5a3e71fe86c2f83ce
Gregor Medoš <106842877+gregormedos@users.noreply.github.com>
2025-04-18 17:55:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bosehubbard',
-    version='0.1',
+    version='1.0',
     description='1D Bose-Hubbard model',
     author='Gregor Medoš',
     packages=find_packages(where="src"),
